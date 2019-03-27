212022aa08861ca72b3235bd2ce3ed4a900c1c8e
Reynaldo Martinez <reynaldomartinez@Reynaldo.local>
2019-03-26 22:29:11 -0400

    Setup project

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SlideShowComponent } from './components/slide-show/slide-show.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AdmissionsComponent } from './pages/admissions/admissions.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { GoogleMapComponent } from './components/google-map/google-map.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SlideShowComponent,
+    HomeComponent,
+    AdmissionsComponent,
+    ResourcesComponent,
+    ContactUsComponent,
+    GoogleMapComponent,
+    ContactFormComponent,
+    VideosComponent,
+    SocialMediaComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
