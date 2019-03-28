7ffbe41303f0280b762ffbf79e7be456d3434c77
Reynaldo Martinez <reynaldozig@hotmail.com>
2019-03-27 21:47:48 -0400

    Add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { AdmissionsComponent } from './pages/admissions/admissions.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'admissions', component: AdmissionsComponent },
+  { path: 'resources', component: ResourcesComponent },
+  // add programs component
+  { path: 'contact-us', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
